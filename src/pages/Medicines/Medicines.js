86c50422fdd165bf7337ef55fdcbde358c502982
HARIKRISHNABIJU15 <you@example.comharikrishnabiju15@gmail.com>
2023-04-25 10:01:30 +0530

    medicine flexx box added

--- src/pages/Medicines/Medicines.js
+++ src/pages/Medicines/Medicines.js
@@ -5,6 +5,35 @@ const Medicines = () => {
   return (
     <MainLayout>
         <h1>Medicines</h1>
+        <div class="flex-container">
+          <div class="flex-box">
+            BOX1
+          </div>
+          <div class="flex-box">
+            BOX2
+          </div>
+          <div class="flex-box">
+            BOX3
+          </div>
+          <div class="flex-box">
+            BOX4
+          </div>
+          <div class="flex-box">
+            BOX5
+          </div>
+          <div class="flex-box">
+            BOX6
+          </div>
+          <div class="flex-box">
+            BOX7
+          </div>
+          <div class="flex-box">
+            BOX8
+          </div>
+          <div class="flex-box">
+            BOX9
+          </div>
+        </div>
         
     </MainLayout>
   )
