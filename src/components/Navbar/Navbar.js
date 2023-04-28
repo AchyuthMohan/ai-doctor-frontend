31f44f12dde96f5a4c190b6255461a1fb838d252
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-04-28 10:51:02 +0530

    nav updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,163 +1,37 @@
-import { useState, React } from "react";
-import { Drawer } from "@mui/material";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./Navbar.css";
-import axiosInstance from "../../auth/authHandler";
-
-
-function Navbar() {
-  const [open, setOpen] = useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const Logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    axiosInstance.defaults.headers["Authorization"] = null;
-    // console.log("user logged out");
-    window.location.reload();
-  };
-
+import { Link } from "react-router-dom";
+// import navIcon from "../../assets/svg/navIcon.svg";
+import { AiOutlineHome } from "react-icons/ai";
+import { GiHospitalCross, GiMedicines, GiBrain } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+const Navbar = () => {
   return (
-    <div className="navbar_1">
-      <div className="nav__desktop">
-        <Link to="/" className="nav_item">
-          <div className="logo">
-            <div className="home_logo"></div>
-            <div className="title">AI Doctor</div>
-          </div>
+    <div className="nav__holder__out">
+      <div className="nav__main_container">
+        <Link to="/">
+          <AiOutlineHome size={30} />
+          Home
         </Link>
-        <div className="navbar_items">
-          <Link to="/" className="nav_item">
-            Home
-          </Link>
-          <Link to="/doctors" className="nav_item">
-            Doctors
-          </Link>
-          <Link to="/medicines" className="nav_item">
-            Medicines
-          </Link>
-          <Link to="/predictor" className="nav_item">
-            Predictor
-          </Link>
-          {localStorage.getItem("refresh_token") ? (
-            <Link
-              className="nav_item_mob"
-              spy={true}
-              smooth={true}
-              onClick={() => {
-                Logout();
-              }}
-            >
-              Logout
-            </Link>
-          ) : (
-            <Link to="/login" className="nav_item_mob" spy={true} smooth={true}>
-              Login
-            </Link>
-          )}
-
-          <Link to="/profile" className="nav_item_mob" spy={true} smooth={true}>
-            Profile
-          </Link>
-        </div>
+        <Link to="/doctors">
+          <GiHospitalCross size={30} />
+          Doctors
+        </Link>
+        <Link to="/medicines">
+          <GiMedicines size={30} />
+          Medicines
+        </Link>
+        <Link to="/predict">
+          <GiBrain size={30} />
+          Predictor
+        </Link>
+        <Link to="/profile">
+          <CgProfile size={30} />
+          Profile
+        </Link>
       </div>
-
-      <div className="nav__mob">
-        <div className="nav__mob_container">
-          <div className="title">AI Doctor</div>
-          <button className="nav_btn" onClick={handleDrawerOpen}>
-            <HiOutlineMenuAlt3 className="hamburger" />
-          </button>
-        </div>
-      </div>
-      <Drawer
-        open={open}
-        onClick={handleDrawerClose}
-        onClose={(event, reason) => {
-          if (reason !== "backdropClick") {
-            handleDrawerClose();
-          } else if (reason !== "escapeKeyDown") {
-            handleDrawerClose();
-          }
-        }}
-        anchor="left"
-      >
-        <div className="nav__drawer">
-          <div className="nav__drawer_header">
-            <div
-              className="title_mob"
-              data-aos="fade-right"
-              data-aos-duration="600"
-            >
-              AI Doctor
-            </div>
-            <div
-              className="navbar_items_mob"
-              data-aos="fade-right"
-              data-aos-duration="600"
-            >
-              <Link to="/" className="nav_item_mob" onClick={handleDrawerClose}>
-                Home
-              </Link>
-              <Link
-                to="/doctors"
-                className="nav_item_mob"
-                spy={true}
-                smooth={true}
-                onClick={handleDrawerClose}
-              >
-                Doctors
-              </Link>
-              <Link
-                to="/medicines"
-                className="nav_item_mob"
-                spy={true}
-                smooth={true}
-                onClick={handleDrawerClose}
-              >
-                Medicines
-              </Link>
-              <Link
-                to="/predictor"
-                className="nav_item_mob"
-                spy={true}
-                smooth={true}
-                onClick={handleDrawerClose}
-              >
-                Predictor
-              </Link>
-
-              <Link
-                to="/login"
-                className="nav_item_mob"
-                spy={true}
-                smooth={true}
-              >
-                Login
-              </Link>
-              {localStorage.getItem("refresh_token") ? (
-                <Link
-                  to="/profile"
-                  className="nav_item_mob"
-                  spy={true}
-                  smooth={true}
-                  onClick={handleDrawerClose}
-                >
-                  Profile
-                </Link>
-              ) : null}
-            </div>
-          </div>
-        </div>
-      </Drawer>
     </div>
   );
-}
+};
 
 export default Navbar;
