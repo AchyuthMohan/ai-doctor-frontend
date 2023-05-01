bc8e974b712e64775006601785887eb7dfd6c976
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-05-01 21:22:08 +0530

    css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './About.css'
+import Bubbles from '../Bubbles/Bubbles'
 const About = () => {
   return (
-    <div>About</div>
+    <Bubbles>
+    <div className='about__main_container'>
+      <img src="" alt="" />
+      <div className="abt_desc">
+        <h1>About</h1>
+        <p>wnnwifwnfiuwnfiuwnfinmi</p>
+      </div>
+    </div>
+    </Bubbles>
   )
 }
 
