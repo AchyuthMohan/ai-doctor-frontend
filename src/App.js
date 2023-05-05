35b69acdb2626422d8451d64100d728d96ec1118
ath1l022 <athulprakash03@gmail.com>
2023-05-05 13:13:28 +0530

    footer and team section created

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
             <div className="btm__vector_c">
               <img src={btmMascotVector} alt="" />
             </div>
+           
             <Bubbles />
           </Suspense>
         </BrowserRouter>
